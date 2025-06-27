72f63d18dab1200104154193597823e2f217b2cf
HamzaOuamrhar <hamzaouamrhar09@gmail.com>
2025-06-27 18:03:05 +0100

    add NER to extract entities from news data

--- spark/kafka_batch_to_postgres.py
+++ spark/kafka_batch_to_postgres.py
@@ -1,8 +1,10 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import from_json, col
-from pyspark.sql.types import StructType, StringType
+from pyspark.sql.functions import from_json, col, explode, udf, concat_ws, trim
+from pyspark.sql.types import StructType, StringType, ArrayType, StructType, StructField
 import os
+import spacy
 
+nlp = spacy.load("en_core_web_sm")
 
 pg_url = os.environ['PG_URL']
 pg_user = os.environ['PG_USER']
@@ -18,8 +20,21 @@ news_schema = (
     .add("content", StringType())
 )
 
+def extract_entities(text):
+    if text is None:
+        return []
+    doc = nlp(text)
+    return [(ent.text, ent.label_) for ent in doc.ents]
+
+entity_schema = ArrayType(StructType([
+    StructField("entity", StringType(), True),
+    StructField("type", StringType(), True)
+]))
+
+extract_entities_udf = udf(extract_entities, entity_schema)
+
 spark = SparkSession.builder \
-    .appName("KafkaNewsBatchToPostgres") \
+    .appName("news") \
     .getOrCreate()
 
 df = spark.read \
@@ -31,10 +46,25 @@ df = spark.read \
     .load()
 
 news_df = df.selectExpr("CAST(value AS STRING) as json_str")
-
 parsed_df = news_df.select(from_json(col("json_str"), news_schema).alias("data")).select("data.*")
 
-parsed_df.write \
+parsed_df = parsed_df.withColumn("title", trim(col("title")))
+parsed_df = parsed_df.withColumn("description", trim(col("description")))
+
+parsed_df = parsed_df.withColumn("full_text", concat_ws(" ", col("title"), col("description")))
+
+entities_df = parsed_df.withColumn("entities", extract_entities_udf(col("full_text")))
+
+flattened_entities_df = entities_df.select(
+    col("publishedAt"),
+    explode(col("entities")).alias("entity_struct")
+).select(
+    col("entity_struct.entity").alias("entity_text"),
+    col("entity_struct.type").alias("entity_type"),
+    col("publishedAt")
+)
+
+flattened_entities_df.write \
     .format("jdbc") \
     .option("url", pg_url) \
     .option("dbtable", pg_table) \
